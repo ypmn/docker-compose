5bef31209735f8d4605c49feb8cd344a4daca484
nethaji <mnethaji999@gmail.com>
2019-07-27 07:43:27 +0000

    docker compose

--- docker-compose-install.sh
+++ docker-compose-install.sh
@@ -1,3 +1,4 @@
+echo "installing the docker compose"
 sudo apt-get update && apt-get install vim -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
